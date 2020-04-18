ae7221fb3f24336ab8975f3bf84d81fa4aa052f6
MILFANPAHLAWAN\milfanmilfan <milfanmilfan@gmail.com>
2020-04-18 10:04:38 +0700

    edit app module

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AccountsModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, AccountsModule ],
   controllers: [AppController],
   providers: [AppService],
 })
