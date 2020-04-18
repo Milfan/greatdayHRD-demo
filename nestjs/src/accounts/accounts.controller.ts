a807332f519f753e2f211527fd48c25be7e9a7be
MILFANPAHLAWAN\milfanmilfan <milfanmilfan@gmail.com>
2020-04-18 10:03:50 +0700

    get data account method

--- nestjs/src/accounts/accounts.controller.ts
+++ nestjs/src/accounts/accounts.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { AuthGuard } from 'src/shared/auth.guard';
 
-@Controller('accounts')
-export class AccountsController {}
+@Controller('api/accounts')
+export class AccountsController {
+    constructor(private service: AccountsService) { }
+
+    @Get()
+    @UseGuards(new AuthGuard())
+    getAllData(){
+        return this.service.getAccount();
+    }
+}
