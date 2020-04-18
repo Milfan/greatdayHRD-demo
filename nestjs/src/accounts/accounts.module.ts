90620272550d65fa6e0da85a2fca659025fbbac3
MILFANPAHLAWAN\milfanmilfan <milfanmilfan@gmail.com>
2020-04-18 10:04:01 +0700

    edit accounts module

--- nestjs/src/accounts/accounts.module.ts
+++ nestjs/src/accounts/accounts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Accounts } from './account.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Accounts])],
   providers: [AccountsService],
   controllers: [AccountsController]
 })
